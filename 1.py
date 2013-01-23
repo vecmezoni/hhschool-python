3dc3b6db886bbf1fa10679d4bcf2c285aa20ea60
Inozemtsev Alexander <a.inozemtsev@me.com>
2013-01-23 19:26:13 +0400

    Added solution for first problem

---
+++ 1.py
@@ -0,0 +1,15 @@
+def curry(function, *args, **kwargs):
+	if (len(args) + len(kwargs)) >= function.__code__.co_argcount:
+		return function(*args, **kwargs)
+	return lambda *x, **y: curry(function, *(args + x), **dict(kwargs, **y))
+
+@curry
+def add(x,y,z,d):
+	return x+y+z+d
+
+assert add(1,2,3,4) == 10
+assert add(1)(2)(3)(4) == 10
+assert add(1,2)(3,4) == 10
+assert add(1,2,3)(4) == 10
+assert add(1)(2,3)(4) == 10
+assert add(x=1)(y=2)(z=3)(d=4) == 10
