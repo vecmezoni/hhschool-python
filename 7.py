480bdaf39de6a722716e454f5e22b77d6f1fc983
Inozemtsev Alexander <a.inozemtsev@me.com>
2013-01-28 00:42:47 +0400

    Fixed solution of seventh problem

--- 7.py
+++ 7.py
@@ -1,38 +1,120 @@
 import inspect
 
-class KeywordMissing(BaseException):
-	pass
-	
-def graph(function, map = {}):
-	if (len(map)) >= function.__code__.co_argcount:
-		for var in function.__code__.co_varnames:
+
+def graph(comp):
+	"""
+	Decorator for graph compiler
+	"""
+	def later(function):
+		return comp(function)
+	return later
+
+def eager_compile(function):
+	"""
+	Simple compiler, computes all values of map
+	"""
+	def compiler(map):
+		variables = inspect.getargspec(function)[0]
+		for var in variables:
 			if not map.has_key(var):
-				raise KeywordMissing(var)
-		result = {}
-		lambdas = function(**map)
-		stack = {}
-		while len(lambdas) > 0 or len(stack) > 0:
-			if len(lambdas) == 0:
-				lambdas = stack
-				stack = {}
-			l = lambdas.popitem()
-			if len(set(inspect.getargspec(l[1])[0]) & map.viewkeys()) < len(inspect.getargspec(l[1])[0]):
-				stack.update({l[0]: l[1]})
-			else:
-				temp = {l[0]: l[1](**{key: map[key] for key in inspect.getargspec(l[1])[0]})}
-				result.update(temp)
-				map.update(temp)
-		return result
-	return lambda map: graph(function, map)	
+				raise KeyError("Key", var, "is missing.")
+		dictionary = function(**map)
+		return {key: evaluate(key, dictionary, map) for key in dictionary}
+	return compiler
 	
-@graph	
+def lazy_compile(function):
+	"""
+	Lazy compiler, computes values by call
+	"""
+	class LazyDictionary(dict):
+		def __init__(self, functions, variables):
+			self.data = variables
+			self.functions = functions
+		def __getitem__(self, name):
+			if not self.data.has_key(name):
+				self.data[name] = evaluate(name, self.functions, self.data)
+			return self.data[name]
+		def __len__(self):
+			return len(self.functions)
+		def __str__(self):
+			return str({key: 'not yet counted' if not self.data.has_key(key) else self.data[key] for key in self.functions})
+		def __iter__(self):
+			return self.iterkeys()
+		def iteritems(self):
+			return iter({key: 'not yet counted' if not self.data.has_key(key) else self.data[key] for key in self.functions}.iteritems())
+		def iterkeys(self):
+			return iter([key for key in self.functions])
+
+	
+	def compiler(map):
+		variables = inspect.getargspec(function)[0]
+		for var in variables:
+			if not map.has_key(var):
+				raise KeyError("Key", var, "is missing.")
+		dictionary = function(**map)
+		return LazyDictionary(dictionary, map)
+	return compiler
+	
+def evaluate(key, dictionary, map = {}):
+	"""
+	Computes value of a variable.
+	"""
+	if dictionary.has_key(key):
+		arguments = inspect.getargspec(dictionary[key])[0]
+	if map.has_key(key):
+		return map[key]
+	else:
+		values = {}
+		for arg in arguments:
+			map[arg] = evaluate(arg, dictionary, map)
+			values[arg] = map[arg]
+		map[key] = dictionary[key](**values)
+		return map[key]
+
+
+@graph(eager_compile)	
 def stats_graph(xs):
 	return {'v': (lambda m, m2: m2 - m**2),
 			'n': (lambda xs: len(xs)),
 			'm': (lambda xs, n: sum(xs) / n),
 			'm2': (lambda xs, n: sum([x**2 for x in xs]) / n)}
+
+assert stats_graph({'xs': [1, 2, 3, 4]}) == {'v': 3, 'n': 4, 'm': 2, 'm2': 7}
+
+def v(m,m2):
+	return m2 - m**2
+
+def n(xs):
+	return len(xs)
+	
+def m(xs, n):
+	return sum(xs) / n
+
+def m2(xs, n):
+	return sum([x**2 for x in xs]) / n
 			
-print stats_graph({'xs': [1,2,3,4]})
+@graph(eager_compile)	
+def stats_graph_no_lambda(xs):
+	return {'v': v,
+			'n': n,
+			'm': m,
+			'm2': m2}
+			
+assert stats_graph_no_lambda({'xs': [1, 2, 3, 4]}) == {'v': 3, 'n': 4, 'm': 2, 'm2': 7}
+
+@graph(lazy_compile)	
+def stats_lazy(xs):
+	return {'v': v,
+			'n': n,
+			'm': m,
+			'm2': m2}
+
+s = stats_lazy({'xs': [1, 2, 3, 4]})
+assert str(s) == "{'v': 'not yet counted', 'n': 'not yet counted', 'm': 'not yet counted', 'm2': 'not yet counted'}"
+assert s['n'] == 4
+assert str(s) == "{'v': 'not yet counted', 'n': 4, 'm': 'not yet counted', 'm2': 'not yet counted'}"
+assert s['v'] == 3
+assert str(s) == "{'v': 3, 'n': 4, 'm': 2, 'm2': 7}"
 
 
 	
