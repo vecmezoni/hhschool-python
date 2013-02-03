d956625283dff2033703d4188a33394d2d5331e6
Inozemtsev Alexander <a.inozemtsev@me.com>
2013-02-03 04:42:49 +0400

    Tests for first problem were rewrited

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 import inspect
+import unittest
 
 def curry(function, *args, **kwargs):
 	"""
@@ -18,23 +19,31 @@ def curry(function, *args, **kwargs):
 	elif len(args) + len(filled) + len(defaults) >= len(pargs):
 		pass
 	return lambda *x, **y: curry(function, *(args + x), **dict(kwargs, **y))
+	
+	
+class TestCurrying(unittest.TestCase):
+	def setUp(self):
+		@curry
+		def add(x, y, z, d = 4, **kwargs):
+			result = x + y + z + d
+			for key in kwargs:
+				result += kwargs[key]
+			return result
+		self.function = add
+	def test_args(self):
+		self.assertEqual(self.function(1, 2, 3, 4), 10)
+		self.assertEqual(self.function(1, 2)(3, 4), 10)
+	def test_defaults(self):
+		self.assertEqual(self.function(1)(2)(3), 10)
+		self.assertEqual(self.function(1, 2, 3), 10)
+		self.assertEqual(self.function(1)(2, 3), 10)
+	def test_kwargs(self):
+		self.assertEqual(self.function(x = 1)(y = 2)(z = 3), 10)
+		self.assertEqual(self.function(kwarg = 5)(1, 2, 3, 4), 15)
+		self.assertEqual(self.function(kwarg = 5)(another_kwarg = 5)(1, 2, 3), 20)
+		self.assertEqual(self.function(kwarg = 5)(1)(2, 3, 4), 15)
+		self.assertEqual(self.function(kwarg = 5)(another_kwarg = 5)(1)(2)(3), 20)
 
-@curry
-def add(x, y, z, d=4, **kwargs):
-	result = x + y + z + d
-	for key in kwargs:
-		result += kwargs[key]
-	return result
-	
-	
-assert add(1, 2, 3, 4) == 10
-assert add(1)(2)(3) == 10
-assert add(1, 2)(3, 4) == 10
-assert add(1, 2, 3) == 10
-assert add(1)(2, 3) == 10
-assert add(x = 1)(y = 2)(z = 3) == 10
-assert add(kwarg = 5)(1, 2, 3, 4) == 15
-assert add(kwarg = 5)(another_kwarg = 5)(1, 2, 3) == 20
-assert add(kwarg = 5)(1)(2, 3, 4) == 15
-assert add(kwarg = 5)(another_kwarg = 5)(1)(2)(3) == 20
+if __name__ == '__main__':
+    unittest.main()	
 
